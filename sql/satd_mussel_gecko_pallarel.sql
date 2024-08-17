f26ed1668a08686b436e2770a039d43eb1e1af5f
miiiiku <w.m124@icloud.com>
2024-08-17 15:59:06 +0900

    sql関係push

--- sql/satd_mussel_gecko_pallarel.sql
+++ sql/satd_mussel_gecko_pallarel.sql
@@ -1,13 +1,13 @@
-DROP TABLE IF EXISTS satd_swt_pallarel.SATD, satd_swt_pallarel.Commits, satd_swt_pallarel.SATDInFile, satd_swt_pallarel.Projects,satd.WaitChange, satd_swt_pallarel.WaitCommits, satd_swt_pallarel.WaitSATD, satd_swt_pallarel.WaitSATDInFile;
+DROP TABLE IF EXISTS satd_gecko_pallarel.SATD, satd_gecko_pallarel.Commits, satd_gecko_pallarel.SATDInFile, satd_gecko_pallarel.Projects,satd_gecko_pallarel.WaitChange, satd_gecko_pallarel.WaitCommits, satd_gecko_pallarel.WaitSATD, satd_gecko_pallarel.WaitSATDInFile;
 
-CREATE TABLE IF NOT EXISTS satd_swt_pallarel.Projects (
+CREATE TABLE IF NOT EXISTS satd_gecko_pallarel.Projects (
 	p_id INT AUTO_INCREMENT NOT NULL,
     p_name VARCHAR(255) NOT NULL UNIQUE,
     p_url VARCHAR(255) NOT NULL UNIQUE,
     PRIMARY KEY (p_id)
 );
 
-CREATE TABLE IF NOT EXISTS satd_swt_pallarel.SATDInFile (
+CREATE TABLE IF NOT EXISTS satd_gecko_pallarel.SATDInFile (
     f_id INT AUTO_INCREMENT NOT NULL,
     f_comment VARCHAR(4096),
     f_comment_type VARCHAR(32),
@@ -20,7 +20,7 @@ CREATE TABLE IF NOT EXISTS satd_swt_pallarel.SATDInFile (
     PRIMARY KEY (f_id)
 );
 
-CREATE TABLE IF NOT EXISTS satd_swt_pallarel.Commits(
+CREATE TABLE IF NOT EXISTS satd_gecko_pallarel.Commits(
     commit_hash varchar(256),
     p_id INT,
     author_name varchar(256),
@@ -33,7 +33,7 @@ CREATE TABLE IF NOT EXISTS satd_swt_pallarel.Commits(
     FOREIGN KEY (p_id) REFERENCES Projects(p_id)
 );
 
-CREATE TABLE IF NOT EXISTS satd_swt_pallarel.SATD (
+CREATE TABLE IF NOT EXISTS satd_gecko_pallarel.SATD (
     satd_id INT AUTO_INCREMENT,
     satd_instance_id varchar(256), -- Not a key value, used only to associate SATD Instances
     parent_instance_id INT,
@@ -45,14 +45,14 @@ CREATE TABLE IF NOT EXISTS satd_swt_pallarel.SATD (
     resolution VARCHAR(64),
     hash_code INT,
     PRIMARY KEY (satd_id),
-    FOREIGN KEY (p_id) REFERENCES satd_swt_pallarel.Projects(p_id),
-    FOREIGN KEY (p_id, first_commit) REFERENCES satd_swt_pallarel.Commits(p_id, commit_hash),
-    FOREIGN KEY (p_id, second_commit) REFERENCES satd_swt_pallarel.Commits(p_id, commit_hash),
-    FOREIGN KEY (first_file) REFERENCES satd_swt_pallarel.SATDInFile(f_id),
-    FOREIGN KEY (second_file) REFERENCES satd_swt_pallarel.SATDInFile(f_id)
+    FOREIGN KEY (p_id) REFERENCES satd_gecko_pallarel.Projects(p_id),
+    FOREIGN KEY (p_id, first_commit) REFERENCES satd_gecko_pallarel.Commits(p_id, commit_hash),
+    FOREIGN KEY (p_id, second_commit) REFERENCES satd_gecko_pallarel.Commits(p_id, commit_hash),
+    FOREIGN KEY (first_file) REFERENCES satd_gecko_pallarel.SATDInFile(f_id),
+    FOREIGN KEY (second_file) REFERENCES satd_gecko_pallarel.SATDInFile(f_id)
 );
 
-CREATE TABLE IF NOT EXISTS satd_swt_pallarel.SummaryFSATD (
+CREATE TABLE IF NOT EXISTS satd_gecko_pallarel.SummaryFSATD (
     satd_id INT AUTO_INCREMENT,
     p_id INT,
     p_name VARCHAR(255) NOT NULL,
@@ -71,7 +71,7 @@ CREATE TABLE IF NOT EXISTS satd_swt_pallarel.SummaryFSATD (
 );
 
 
-CREATE TABLE IF NOT EXISTS satd_swt_pallarel.WaitChange (
+CREATE TABLE IF NOT EXISTS satd_gecko_pallarel.WaitChange (
     wait_id INT AUTO_INCREMENT,
     parent_hashcode INT,
     new_hash_code INT,
@@ -85,7 +85,7 @@ CREATE TABLE IF NOT EXISTS satd_swt_pallarel.WaitChange (
     );
 
 
-CREATE TABLE IF NOT EXISTS satd_swt_pallarel.WaitSATDInFile (
+CREATE TABLE IF NOT EXISTS satd_gecko_pallarel.WaitSATDInFile (
     f_id INT AUTO_INCREMENT NOT NULL,
     f_comment VARCHAR(4096),
     f_comment_type VARCHAR(32),
@@ -99,7 +99,7 @@ CREATE TABLE IF NOT EXISTS satd_swt_pallarel.WaitSATDInFile (
     );
 
 
-CREATE TABLE IF NOT EXISTS satd_swt_pallarel.WaitCommits(
+CREATE TABLE IF NOT EXISTS satd_gecko_pallarel.WaitCommits(
     commit_hash varchar(256),
     p_id INT,
     author_name varchar(256),
@@ -111,7 +111,7 @@ CREATE TABLE IF NOT EXISTS satd_swt_pallarel.WaitCommits(
     PRIMARY KEY (p_id, commit_hash)
     );
 
-CREATE TABLE IF NOT EXISTS satd_swt_pallarel.WaitSATD (
+CREATE TABLE IF NOT EXISTS satd_gecko_pallarel.WaitSATD (
     satd_id INT AUTO_INCREMENT,
     parent_instance_id INT,
     p_id INT,
